1ee80d2cab388d3bf3430a2d60c3c4b5a42ac92d
Jason Chan <jasonchanhw0531@gmail.com>
2024-05-30 19:57:43 +0100

    added styling to login page

--- reminders-frontend/src/pages/LoginPage.tsx
+++ reminders-frontend/src/pages/LoginPage.tsx
@@ -7,12 +7,22 @@ const LoginPage = () => {
 
     return (
         <div className='login-div'>
-            <form onSubmit={loginUser}>
-                <input type="text" name="username" placeholder="Username"/>
-                <input type="password" name="password" placeholder="Password"/>
-                <input type="submit"/>
+            <form onSubmit={loginUser} className='login-form'>
+                <h2>Login</h2>
+                <div className='login-form-item'>
+                    <label htmlFor='username'>Username:</label>
+                    <input type="text" id ='username' name="username" placeholder="Username"/>
+                </div>
+                
+                <div className='login-form-item'>
+                    <label htmlFor='password'>Password: </label>
+                    <input type="password" name="password" placeholder="Password"/>
+                </div>
+
+                <input type="submit" className='login-button' value='Login'/>
+                <div className='login-message'>{message}</div>
             </form>
-            <div>{message}</div>
+            
         </div>
     )
 }
